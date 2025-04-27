4619815bf4d1e5ae3a6e41d51262678dad7dc4cb
Dmytro Lobanov <dspwww@gmail.com>
2025-04-27 16:43:28 +0100

    feat: Add fibs function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello')
+console.log('Hello');
+
+function fibs(n) {
+  let result = [];
+
+  for(let i = 0; i < n; i++) {
+    if(result.length < 2) {
+      result.push(result.length === 0 ? 0 : 1);
+    } else {
+      result.push(result[i-1] + result[i-2]);
+    }
+  }
+
+  return result;
+}
+
+console.log(fibs(8));
+
